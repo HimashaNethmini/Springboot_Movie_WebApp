2df7d4438673c65f411932501ef933b847e424af
HimashaNethmini <himashanethmini05@gmail.com>
2023-12-24 15:45:08 +0530

    get single movie using Imdbid

--- movies/src/main/java/com/himasha/movies/MovieService.java
+++ movies/src/main/java/com/himasha/movies/MovieService.java
@@ -3,7 +3,6 @@ package com.himasha.movies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,7 +15,7 @@ public class MovieService {
         return movieRepository.findAll();
     }
 
-    //get single movie using Imdbid
+    //get  single movie using Imdbid
     public Optional<Movie> findMovieByImdbId(String imdbId) {
         return movieRepository.findMovieByImdbId(imdbId);
     }

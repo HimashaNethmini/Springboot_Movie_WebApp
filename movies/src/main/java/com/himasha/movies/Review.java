ca78f7f27a10b5de38e8e519edbfa9ccdb533400
HimashaNethmini <himashanethmini05@gmail.com>
2023-12-28 09:50:23 +0530

    review class

--- movies/src/main/java/com/himasha/movies/Review.java
+++ movies/src/main/java/com/himasha/movies/Review.java
@@ -5,8 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
 
 @Document (collection = "reviews")
 @Data
@@ -14,10 +15,15 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @NoArgsConstructor
 
 public class Review {
-    @Id
+
     private ObjectId id;
     private String body;
+    private LocalDateTime created;
+    private LocalDateTime updated;
 
-
-
+    public Review(String body, LocalDateTime created, LocalDateTime updated) {
+        this.body = body;
+        this.created = created;
+        this.updated = updated;
+    }
 }
